0248531ff178c7986bb37e5e4c63acee2f0d7143
Sean Perez <sean.perez29@gmail.com>
2016-12-04 18:47:33 -0800

    feat: Add Apple's AVCam Objective-C code to project

--- DevChat/CameraVC.swift
+++ DevChat/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  DevChat
 //
 //  Created by Sean Perez on 12/4/16.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
